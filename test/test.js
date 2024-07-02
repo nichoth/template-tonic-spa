5172bb137efdaafd45d7c63b0054b3ece28cf481
Nick Thomas <nichoth@nichoth.com>
2024-07-01 20:42:59 -0700

    wip

--- test/test.js
+++ test/test.js
@@ -1,5 +1,6 @@
 // @ts-check
 import { test } from '@bicycle-codes/tapzero'
+import '../src/index.js'
 
 test('find an element', async t => {
     t.ok(document.querySelector('h1'), 'should find an h1')
